fb128261287fe7a5254b7b828a7f0e2bf2b10a8f
Elmeri Niemelä <niemela.elmeri@gmail.com>
2019-06-24 12:47:20 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.svg";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">
+              <img src={logo} width="30" height="30" alt="Missing"/>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class TodosList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>TodosList</h2>
+      </div>
+    );
+  }
+}
